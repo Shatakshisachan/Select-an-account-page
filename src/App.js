100c8000814daad146db7df62d8c97808bfd0186
Shatakshisachan <49412767+Shatakshisachan@users.noreply.github.com>
2021-01-25 13:06:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import Names from "./Names";
 export default function App() {
   return (
     <div className="App">
+      <div className = "heading">
       <h1>Select an account</h1>
+      </div>
+      <div id="listOfNames">
       <Names />
+      </div>
     </div>
   );
 }
